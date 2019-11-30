187495542ef77009f46470c9db0c0482938394a5
ZhangZisu <admin@zhangzisu.cn>
2019-11-30 19:11:12 +0800

    :beers: Add some options

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,12 +1,21 @@
 import { createConnection } from 'typeorm'
 import { User } from './user'
 import { Device } from './device'
+import { argv } from 'yargs'
+
+const type = (argv.dbType || 'mysql') as 'mysql'
+const database = (argv.db || 'test') as string
+const username = (argv.dbUser || 'test') as string
+const password = (argv.dbPass || 'password') as string
+
+console.log('Database Type : ' + type)
+console.log('Database      : ' + database)
 
 createConnection({
-  type: 'mysql',
-  database: 'test',
-  username: 'test',
-  password: 'password',
+  type,
+  database,
+  username,
+  password,
   entities: [User, Device],
   synchronize: true
 }).then(() => {
